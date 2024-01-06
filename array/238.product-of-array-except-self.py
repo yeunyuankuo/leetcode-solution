577e3c4718a888c8491adc6e9dcbba11367ea5f7
jessiekuo <yeunyuankuo@gmail.com>
2024-01-06 22:55:29 +0800

    238.product-of-array-except-self added

---
+++ array/238.product-of-array-except-self.py
@@ -0,0 +1,14 @@
+def productExceptSelf(self, nums: list[int]) -> list[int]:
+    # time: O(N)
+    # space: O(1)
+    res = [1] * len(nums)
+
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums)-1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
