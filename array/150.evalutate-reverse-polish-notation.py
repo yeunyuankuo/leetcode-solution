37a9022d470e6a7e65af9664ac62f59e3a80eab7
jessiekuo <yeunyuankuo@gmail.com>
2024-01-10 22:15:27 +0800

    150 added

---
+++ array/150.evalutate-reverse-polish-notation.py
@@ -0,0 +1,18 @@
+def evalRPN(self, tokens: list[str]) -> int:
+    # time: O(N)
+    # space: O(N)
+    stack = []
+    for c in tokens:
+        if c == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif c == '-':
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif c == '/':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(b / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
