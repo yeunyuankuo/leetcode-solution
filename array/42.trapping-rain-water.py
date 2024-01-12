beb48feedc8efd5a519b87ad3085da381c6d4231
jessiekuo <yeunyuankuo@gmail.com>
2024-01-12 22:40:54 +0800

    add 42

---
+++ array/42.trapping-rain-water.py
@@ -0,0 +1,20 @@
+def trap(self, height: list[int]) -> int:
+    # time: O(N)
+    # space: O(N)
+    res = 0
+    l, r = 0, len(height)-1
+    maxL, maxR = height[0], height[-1]
+
+    while l < r:
+        if height[l] < height[r]:
+            currWater = maxL - height[l]
+            res += max(0, currWater)
+            maxL = max(maxL, height[l])
+            l += 1
+        else:
+            currWater = maxR - height[r]
+            res += max(0, currWater)
+            maxR = max(maxR, height[r])
+            r -= 1
+
+    return res
