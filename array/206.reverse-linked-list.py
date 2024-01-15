51fb360a0483f0b844506c7c46493a4133d019e9
jessiekuo <yeunyuankuo@gmail.com>
2024-01-15 23:16:56 +0800

    add 206

---
+++ array/206.reverse-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    # time: O(N)
+    # space: O(1)
+    curr, prev = head, None
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    
+    return prev
