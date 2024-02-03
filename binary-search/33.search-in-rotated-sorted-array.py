37f070766a746b1e52366c0cc13b5af9c5af61ca
jessiekuo <yeunyuankuo@gmail.com>
2024-02-03 18:23:20 +0800

    update 33

--- binary-search/33.search-in-rotated-sorted-array.py
+++ binary-search/33.search-in-rotated-sorted-array.py
@@ -1,24 +1,24 @@
 def search(self, nums: list[int], target: int) -> int:
     # time: O(logN)
     # space: O(1)
-    l, r = 0, len(nums) - 1
-    
+    l, r = 0, len(nums)-1
     while l <= r:
-        mid = l + (r - l) // 2
+        mid = (l + r) // 2
         if nums[mid] == target:
             return mid
 
-        # left sorted portion
+        # case 1: subarray on mid's left is sorted
         if nums[l] <= nums[mid]:
-            if target > nums[mid] or target < nums[l]:
-                l = mid + 1
-            else:
+            if target > nums[l] and target < nums[mid]:
                 r = mid - 1
-        # right sorted portion
+            else:
+                l = mid + 1
+
+        # case 2: subarray on mid's right is sorted
         else:
-            if target < nums[mid] or target > nums[r]:
-                r = mid - 1
-            else:
+            if target < nums[r] and target > nums[mid]:
                 l = mid + 1
-                
+            else:
+                r = mid - 1
+
     return -1
