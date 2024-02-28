19759ec7eeacd6aa3d281a4b766c1eae97158467
jessiekuo <yeunyuankuo@gmail.com>
2024-02-28 16:31:50 +0800

    update 79

--- backtracking/79.word-search.py
+++ backtracking/79.word-search.py
@@ -1,5 +1,13 @@
 class Solution:
-    # Time Complexity: O(m * n * 4^k)
+    # Time Complexity: O(m * n * 4^k), m: the number of rows in the board.
+    # n: the number of columns in the board. k: the length of the word.
+    # The main function exist iterates over all cells of the board, which takes O(m * n) time.
+    # Inside the dfs function, each cell is visited at most once, and each recursive call
+    # explores up to four neighboring cells. The maximum number of recursive calls for
+    # each cell is bounded by the length of the word k. Hence, the time complexity of
+    # the DFS part of the algorithm is O(4^k). Overall, the time complexity is
+    # dominated by the DFS traversal, so it is O(m * n * 4^k).
+    #
     # Space Complexity: O(k)
     def exist(self, board: list[list[str]], word: str) -> bool:
         w = len(board)
