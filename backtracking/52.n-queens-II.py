0a56887ab3b39fec37999dee64a6d2499486b1a1
jessiekuo <yeunyuankuo@gmail.com>
2024-03-02 15:54:25 +0800

    update n queen

--- backtracking/52.n-queens-II.py
+++ backtracking/52.n-queens-II.py
@@ -1,19 +1,19 @@
 class Solution:
-    # time:time: O(N * N!), b/c N is the number of cells a queen can choice in a row and
-    # the N-1 * N-2 * N-3 ... is the number of configurations to check for the cells in different row
-    # space: O(N^2)
-    def solveNQueens(self, n: int) -> list[list[str]]:
-        board = [ ["."] * n for _ in range(n)]
-        res = []
+    def totalNQueens(self, n: int) -> int:
+        # time: O(N * N!), b/c N is the number of cells a queen can choice in a row and
+        # the N-1 * N-2 * N-3 ... is the number of configurations to check for the cells in different row
+        # space: O(N), for maintaining cols and posDiag and negDiag sets
+        res = 0
 
         cols = set()
         posDiag = set()
         negDiag = set()
 
         def bt(r):
+            nonlocal res
+
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+                res += 1
                 return
 
             for c in range(n):
@@ -23,14 +23,12 @@ class Solution:
                 cols.add(c)
                 posDiag.add(r + c)
                 negDiag.add(r - c)
-                board[r][c] = "Q"
 
                 bt(r + 1)
 
                 cols.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
-                board[r][c] = "."
 
         bt(0)
         return res
